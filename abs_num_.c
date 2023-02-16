747d2a17076584ebf66fc6629aa3839992ccfff9
ShubhamDubey0785 <114800339+ShubhamDubey0785@users.noreply.github.com>
2023-02-16 23:40:25 +0530

    Add files via upload

---
+++ abs_num_.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+    int user;
+    printf("User enter their number here: ");
+    scanf("%d",&user);
+    printf("The absolute value of the number enter by the user is %d",abs(user));
+    return 0;
+}
