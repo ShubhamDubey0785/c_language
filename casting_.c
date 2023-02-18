3134a132412deb543d6ff07a543c38ec82d9a5eb
ShubhamDubey0785 <114800339+ShubhamDubey0785@users.noreply.github.com>
2023-02-18 21:15:42 +0530

    Add files via upload

---
+++ casting_.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter your first number: \n");
+    scanf("%d",&num1);
+    printf("Enter you second number: \n");
+    scanf("%d",&num2);
+    printf("the first number is %d and second number is %d and25 after applyig mathematical operation we will find %d",num1,num2,num1/num2);
+    return 0;
+}
