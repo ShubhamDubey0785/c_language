99a17b8c2a92b03cdc00f752de43e734f39c9596
ShubhamDubey0785 <114800339+ShubhamDubey0785@users.noreply.github.com>
2023-02-18 21:15:21 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d + %d = %d\n",5,2,5+2);
+    printf("%d - %d = %d\n",5,2,5-2);
+    printf("%d * %d = %d\n",5,2,5*2);
+    printf("%d / %d = %d\n",5,2,5/2);
+    printf("%d %% %d = %d\n",5,2,5%2);
+    return 0;
+}
